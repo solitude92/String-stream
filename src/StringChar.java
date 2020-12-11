d3564d7c2b0869554647f665ba859a1d7562f5fb
solitude92 <m.solitude92@gmail.com>
2020-12-11 14:13:31 +0330

    using streams

--- src/StringChar.java
+++ src/StringChar.java
@@ -1,10 +1,17 @@
 import java.lang.reflect.Array;
 import java.util.Arrays;
+import java.util.List;
+import java.util.Map;
 import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public class StringChar {
     public static void main(String[] args) {
         String[] array = {"Absolutely", "Hi", "Hello", "Howdy Buddy", "Fine", "RightThings"};
-        System.out.println(Arrays.stream(array).collect(Collectors.groupingBy(String::length)));
+
+        System.out.println(Arrays.stream(array). collect(Collectors.groupingBy(String::length )));
+        System.out.println(Arrays.stream(array). collect(Collectors.groupingBy(String::length, Collectors.counting())));
+
+
     }
 }
